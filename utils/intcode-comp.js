fe8eec245a26004504617f0689f10bc44e332380
Devin Moore <dmoore@fivestardev.com>
2020-10-23 09:04:45 -0400

    start "enhancing" intcode computer

--- utils/intcode-comp.js
+++ utils/intcode-comp.js
@@ -1,5 +1,29 @@
-
 const listUtils = require('./list-utils')
+
+const operations = {
+    add: 1,
+    multiply: 2,
+    address: 3,
+    value: 4
+};
+
+const reverse = function (string) {
+    return string.split('').reverse().join('');
+}
+
+const toOpCodes = function (value) {
+    const _value = value.toString();
+    const operation = _value.slice(_value.length - 2);
+    const parameters =
+        reverse(_value.slice(0, _value.indexOf(operation)))
+            .split('')
+            .map(v => parseInt(v));
+
+    return {
+        operation: parseInt(operation),
+        parameters
+    };
+}
 
 exports.calculate = function (integers, noun, verb) {
     const _integers = listUtils.copy(integers);
@@ -11,7 +35,7 @@ exports.calculate = function (integers, noun, verb) {
         .splitIntoGroups(_integers, 4)
         .map(group => {
             return {
-                operation: group[0],
+                opCode: toOpCodes(group[0]),
                 input1Position: group[1],
                 input2Position: group[2],
                 outputPosition: group[3]
@@ -23,11 +47,11 @@ exports.calculate = function (integers, noun, verb) {
         const opCode = opCodes[i];
         let haltProgram = false;
         let value;
-        switch (opCode.operation) {
-            case 1:
+        switch (opCode.opCode.operation) {
+            case operations.add:
                 value = _integers[opCode.input1Position] + _integers[opCode.input2Position];
                 break;
-            case 2:
+            case operations.multiply:
                 value = _integers[opCode.input1Position] * _integers[opCode.input2Position];
                 break;
             case 99:
